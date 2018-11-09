4567d0d1f3087225b4cd05d9ebbbc389ccc0618f
Alex Brown <alexdbrown2526@gmail.com>
2018-11-09 15:28:52 -0600

    shooter registration page created and post route working

--- server/routes/competition/shooter.router.js
+++ server/routes/competition/shooter.router.js
@@ -13,7 +13,8 @@ router.get('/', (req, res) => {
  * POST route template
  */
 router.post('/', (req, res) => {
-
+    pool.query(`INSERT INTO "shooter" ("first_name", "last_name", "email", "phone", "handicap", "ata_number")
+    VALUES ($1, $2, $3, $4, $5, $6);`, [req.body.firstName, req.body.lastName, req.body.email, req.body.phone, req.body.handicap, req.body.ataNumber])
 });
 
 module.exports = router;
