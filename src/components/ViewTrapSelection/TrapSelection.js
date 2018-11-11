4571c0d6fd57df98b134f1e32d9743d038f60d84
brdrummer <brdrummer@hotmail.com>
2018-11-11 17:26:56 -0600

    Basic page creation

---
+++ src/components/ViewTrapSelection/TrapSelection.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+
+class TrapSelection extends Component {
+  state = {
+    value: "",
+    isVisible: false
+  };
+
+  // TODO: Populate dropdown with DB traps, currently getting an error that stops at the saga.
+
+  componentDidMount() {
+    this.props.dispatch({ type:'FETCH_TRAPS', payload: ''});
+ }
+
+  handleChangeFor = propertyName => event => {
+    this.setState({
+        ...this.state,
+        [propertyName]: event.target.value
+    });
+};
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      ...this.state,
+      isVisible: !this.state.isVisible
+    });
+  }
+
+  render() {
+    let displayItem;
+    if (this.state.isVisible) {
+      displayItem = this.props.history.push("/scoring");
+    }
+    return (
+      <div>
+        <h1>Trap Selection</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <select onChange={this.handleChangeFor('propertyName')}>
+              <option value="Trap One">Trap One</option>
+              <option value="Trap Two">Trap Two</option>
+              <option value="Trap Three">Trap Three</option>
+              <option value="Trap Four">Trap Four</option>
+            </select>
+          </label>
+          <input type="submit" value="Confirm" />
+        </form>
+        {displayItem}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = reduxState => ({
+ 
+    reduxState
+});
+
+export default connect(mapStateToProps)(TrapSelection);
