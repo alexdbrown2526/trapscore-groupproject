e325a86e3d9be8294b0142b24f2a888864203a8e
Joseph Carey <josephcareycomposer@gmail.com>
2018-11-11 15:48:19 -0600

    create basic sagas to get info for each reducer from the server

---
+++ src/redux/sagas/events.saga.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+const sagaName = 'eventsSaga'
+
+function* fetchEvents() {
+  try {
+    const response = yield axios.get('api/competition/events');
+
+    yield put({ type: 'SET_EVENTS', payload: response.data });
+  } catch (error) {
+    console.log('error in', sagaName, ':', error);
+  }
+}
+
+function* eventsSaga() {
+  yield takeLatest('FETCH_EVENTS', fetchEvents);
+}
+
+export default eventsSaga;
---
+++ src/redux/sagas/traps.saga.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+const sagaName = 'trapsSaga'
+
+function* fetchTraps() {
+  try {
+    const response = yield axios.get('api/competition/traps');
+
+    yield put({ type: 'SET_TRAPS', payload: response.data });
+  } catch (error) {
+    console.log('error in', sagaName, ':', error);
+  }
+}
+
+function* trapsSaga() {
+  yield takeLatest('FETCH_TRAPS', fetchTraps);
+}
+
+export default trapsSaga;
