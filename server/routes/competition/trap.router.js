a2108972ca47d19b58debfa7b7d9b9e0ccd72067
Christopher Brenberg <cbrenberg@gmail.com>
2018-11-11 16:14:23 -0600

    add /competition/trap GET route

--- server/routes/competition/trap.router.js
+++ server/routes/competition/trap.router.js
@@ -3,10 +3,14 @@ const pool = require("../../modules/pool");
 const router = express.Router();
 
 /**
- * GET route template
+ * GET a list of all traps from currently selected competition
  */
 router.get('/', (req, res) => {
-    
+    pool.query(`SELECT "trap"."id", "trap"."name" FROM "trap" 
+    JOIN "squad_trap" on "squad_trap"."trap_id" = "trap"."id"
+    JOIN "squad" on "squad_trap"."squad_id" = "squad"."id"
+    JOIN "event" on "squad"."event_id" = "event"."id"
+    WHERE "event"."competition_id" = ${req.user.competition_id};`);
 });
 
 /**
