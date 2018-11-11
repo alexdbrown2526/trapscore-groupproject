86f4ec6f57edcc6f51a58f412c6eb68488cd3044
Joseph Carey <josephcareycomposer@gmail.com>
2018-11-11 15:26:22 -0600

    update the redux store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
-import errors from './errorsReducer';
-import loginMode from './loginModeReducer';
-import user from './userReducer';
+import errors from './login/errors.reducer';
+import loginMode from './login/loginMode.reducer';
+import user from './login/user.reducer';
+import shooters from './shooters.reducer';
+import traps from './traps.reducer';
+import events from './events.reducer';
+import squaddingData from './squaddingData.reducer';
+import schedulingData from './schedulingData.reducer';
+import selectedTrap from './selectedTrap.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -13,6 +19,12 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
   user, // will have an id and username if someone is logged in
+  shooters,
+  traps,
+  events,
+  squaddingData,
+  schedulingData,
+  selectedTrap,
 });
 
 export default rootReducer;
