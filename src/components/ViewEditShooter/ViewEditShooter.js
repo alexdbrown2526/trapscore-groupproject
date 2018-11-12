d7cd6a20064463188bd57cbf1fd23c186b2a9154
Christopher Brenberg <cbrenberg@gmail.com>
2018-11-12 11:45:46 -0600

    change willReceiveProps to componentDidUpdate

--- src/components/ViewEditShooter/ViewEditShooter.js
+++ src/components/ViewEditShooter/ViewEditShooter.js
@@ -1,90 +1,91 @@
-import React, {Component} from 'react'
-import axios from 'axios'
-
+import React, { Component } from "react";
+import axios from "axios";
 
 class ViewEditShooter extends Component {
-        constructor(props){
-        super(props);
-        this.state = {
-            first_name: '',
-            last_name: '',
-            email: '',
-            handicap: '',
-            phone: '',
-            ata_number: '',
-        }
-     }
+  constructor(props) {
+    super(props);
+    this.state = {
+      first_name: "",
+      last_name: "",
+      email: "",
+      handicap: "",
+      phone: "",
+      ata_number: ""
+    };
+  }
 
-   
+  updateUser = id => {
+    console.log("update button working");
+    axios({
+      method: "PUT",
+      url: `/api/competition/shooter/${id}`
+      // data: updatedUserInfo
+    });
+  };
 
-    updateUser = (id) => {
-        console.log('update button working');
-        axios({
-            method: 'PUT',
-            url: `/api/competition/shooter/${id}`,
-            // data: updatedUserInfo
+  handleChangeFor = propertyName => event => {
+    this.setState({
+      ...this.state,
+      [propertyName]: event.target.value
+    });
+  };
 
-        })
-        
+  componentDidUpdate(prevProps) {
+    if (this.props != prevProps) {
+      this.setState({
+        ...this.props.selectedShooter
+      });
     }
+  }
 
-    handleChangeFor = propertyName => (event) => {
-        this.setState({
-            ...this.state,
-           [propertyName]: event.target.value
-        });
-
-    }
-
-    componentWillReceiveProps(){
-        this.setState({
-            ...this.props.selectedShooter
-        })
-    }
-
-
-    render(){
-        return(
-            <div>
-                <form className="form">
-				<input className="textfield"
-					type="text"
-					name='searchText'
-					value={this.state.first_name}
-					onChange={this.handleChangeFor('first_name')}
-				 />
-                 <input className="textfield"
-					type="text"
-					name='searchText'
-					value={this.state.last_name}
-					onChange={this.handleChangeFor('last_name')}
-				 />
-                 <input className="textfield"
-					type="text"
-					name='searchText'
-					value={this.state.email}
-					onChange={this.handleChangeFor('email')}
-				 />
-                 <input className="textfield"
-					type="text"
-					name='searchText'
-					value={this.state.phone}
-					onChange={this.handleChangeFor('phone')}
-				 />
-                 <input className="textfield"
-					type="text"
-					name='searchText'
-					value={this.state.handicap}
-					onChange={this.handleChangeFor('handicap')}
-				 />
-                 <input className="textfield"
-					type="text"
-					name='searchText'
-					value={this.state.ata_number}
-					onChange={this.handleChangeFor('ata_number')}
-				 />
-                 </form>
-                {/* // <ul>
+  render() {
+    return (
+      <div>
+        <form className="form">
+          <input
+            className="textfield"
+            type="text"
+            name="searchText"
+            value={this.state.first_name}
+            onChange={this.handleChangeFor("first_name")}
+          />
+          <input
+            className="textfield"
+            type="text"
+            name="searchText"
+            value={this.state.last_name}
+            onChange={this.handleChangeFor("last_name")}
+          />
+          <input
+            className="textfield"
+            type="text"
+            name="searchText"
+            value={this.state.email}
+            onChange={this.handleChangeFor("email")}
+          />
+          <input
+            className="textfield"
+            type="text"
+            name="searchText"
+            value={this.state.phone}
+            onChange={this.handleChangeFor("phone")}
+          />
+          <input
+            className="textfield"
+            type="text"
+            name="searchText"
+            value={this.state.handicap}
+            onChange={this.handleChangeFor("handicap")}
+          />
+          <input
+            className="textfield"
+            type="text"
+            name="searchText"
+            value={this.state.ata_number}
+            onChange={this.handleChangeFor("ata_number")}
+          />
+        </form>
+        {/* // <ul>
                 //     <li>
                 //         {JSON.stringify(this.props.selectedShooter[0])}
                 //         {this.props.selectedShooter.first_name}
@@ -105,10 +106,10 @@ class ViewEditShooter extends Component {
                 //         {this.props.selectedShooter.ata_number}
                 //     </li>
                 // </ul> */}
-                <button onClick={this.updateUser}>Save Changes</button>
-            </div>
-        )
-    }
+        <button onClick={this.updateUser}>Save Changes</button>
+      </div>
+    );
+  }
 }
 
-export default ViewEditShooter
+export default ViewEditShooter;
