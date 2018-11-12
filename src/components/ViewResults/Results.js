a26d11aafcadbe38772b3ba218b26a0f904b6552
Christopher Brenberg <cbrenberg@gmail.com>
2018-11-12 16:53:57 -0600

    add basic results table view

--- src/components/ViewResults/Results.js
+++ src/components/ViewResults/Results.js
@@ -1,13 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ReactTable from 'react-table';
+import "react-table/react-table.css";
 
 class Results extends Component {
+  state = {
+    page: 1,
+  };
+
+  paginateOnASchedule = () => {
+    this.setState({
+      page: this.state.page + 1,
+    })
+  }
 
   render() {
+
+    const columns = [{
+      Header: 'Shooter Name',
+      accessor: 'name', //change later based on server response
+    }, {
+      Header: 'Total Score',
+      accessor: 'score',
+      //Cell: render function to include score out of 100 shots
+    }, {
+      Header: 'Placement',
+      accessor: 'placement', //change this after 
+    }];
+
+    const data = [{
+      name: 'Luke Schlangen',
+      score: 42,
+      placement: 1,
+    }]
+
     return (
-     <h1>Results</h1>
-    )
+      <>
+        <h1>Results</h1>
+        {/* toggle button component */}
+        <button>Toggles Go Here</button>
+        {/* react-table component */}
+        <ReactTable
+          columns={columns}
+          data={data}
+          pageSize={15}
+        />
+      </>
+    );
   }
 }
-
 
 export default Results;
