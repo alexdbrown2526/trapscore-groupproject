e854e98f35e4a28444aeee91a39ca1c881904ab7
Joseph Carey <josephcareycomposer@gmail.com>
2018-11-12 16:51:19 -0600

    start progress toward sending result data

--- server/routes/competition/results.router.js
+++ server/routes/competition/results.router.js
@@ -1,19 +1,23 @@
-const express = require('express');
+const express = require("express");
 const pool = require("../../modules/pool");
 const router = express.Router();
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-    
+router.get("/", (req, res) => {
+  console.log("Results router hit");
+  console.log(req.user);
+  let competition_id = req.user.competition_id;
+  console.log(competition_id);
+  const toSend = "cool";
+
+  res.send(toSend);
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-
-});
+router.post("/", (req, res) => {});
 
 module.exports = router;
