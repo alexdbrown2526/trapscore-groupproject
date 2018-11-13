305de9843a5ee6ccc5ddd608de5a40797a05d0f1
Joseph Carey <josephcareycomposer@gmail.com>
2018-11-13 15:44:30 -0600

    fix the doubles dummy data

--- makeScores.js
+++ makeScores.js
@@ -29,3 +29,17 @@ for (toScore of toScore25) {
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
+/* DO NOT DELETE
+
+SELECT 
+  "squad_trap"."id" as "squad_trap_id",
+  "shooter_event"."id" as "shooter_event_id" FROM "squad_trap"
+JOIN "squad" ON "squad_trap"."squad_id" = "squad"."id"
+JOIN "shooter_squad" ON "squad"."id" = "shooter_squad"."squad_id"
+JOIN "event" ON "squad"."event_id" = "event"."id" 
+JOIN "shooter_event" ON "event"."id" = "shooter_event"."event_id"
+JOIN "shooter" ON "shooter_event"."shooter_id" = "shooter"."id" AND "shooter_squad"."shooter_id" = "shooter"."id"
+;
+
+*/
