50f8b72e29d85c522df2199b6a1439ffec64ba63
Christopher Brenberg <cbrenberg@gmail.com>
2018-11-13 11:41:26 -0600

    add event/:comp_id get route, fix shooter post route and register form

--- server/routes/competition/event.router.js
+++ server/routes/competition/event.router.js
@@ -12,4 +12,14 @@ router.get('/', (req, res) => {
       })
 });
 
+// GET a list of all events associated with current competition
+router.get('/:comp_id', (req, res) => {
+  pool.query(`SELECT * FROM "event" WHERE "competition_id" = ${req.params.id}`)
+    .then(results => res.send(results.rows))
+    .catch(error => {
+      console.log('Error getting events from /api/competition/event', error);
+      res.sendStatus(500);
+    })
+});
+
 module.exports = router;
