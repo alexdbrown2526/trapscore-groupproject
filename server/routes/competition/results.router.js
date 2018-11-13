2bf2544e8fc42b8e846c91c5592e50ae1b6b808a
Joseph Carey <josephcareycomposer@gmail.com>
2018-11-13 11:24:31 -0600

    create a server router that provides results

--- server/routes/competition/results.router.js
+++ server/routes/competition/results.router.js
@@ -6,13 +6,57 @@ const router = express.Router();
  * GET route template
  */
 router.get("/", (req, res) => {
-  console.log("Results router hit");
-  console.log(req.user);
-  let competition_id = req.user.competition_id;
-  console.log(competition_id);
-  const toSend = "cool";
+  // let competition_id = req.user.competition_id;
+  let competition_id = 1;
+  let toSend = [];
 
-  res.send(toSend);
+  // Get a list of all of the events for the specific competition
+  pool
+    .query(
+      `
+      SELECT
+        *
+      FROM "event"
+      WHERE "competition_id" = $1;
+    `,
+      [competition_id]
+    )
+    .then(results => {
+      toSend = results.rows;
+      let promises = [];
+      // loop through the events and get the participants and their results
+      // TODO: ensure these are in order?
+      for (let currentEvent of toSend) {
+        promises.push(
+          pool
+            .query(
+              `
+              SELECT
+                "shooter"."first_name",
+                "shooter"."last_name",
+                SUM("score"."score") AS "total_hits",
+                COUNT("score") AS "total_shots",
+                json_agg("score") AS "raw_scores"
+              FROM "event"
+              JOIN "shooter_event" ON "event"."id" = "shooter_event"."event_id"
+                JOIN "score" ON "shooter_event"."id" = "score"."shooter_event_id"
+                JOIN "shooter" ON "shooter_event"."shooter_id" = "shooter"."id"
+              WHERE "event_id" = $1
+              GROUP BY "event"."id", "shooter"."id"
+              ;
+            `,
+              [currentEvent.id]
+            )
+            .then(results => {
+              currentEvent.results = results.rows;
+            })
+        );
+      }
+
+      Promise.all(promises).then(() => {
+        res.send(toSend);
+      });
+    });
 });
 
 /**
