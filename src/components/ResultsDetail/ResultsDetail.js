68a0903d775dd4aea06073fa02b9f3d060e47e49
Christopher Brenberg <cbrenberg@gmail.com>
2018-11-14 22:38:36 -0600

    add results detail styling

--- src/components/ResultsDetail/ResultsDetail.js
+++ src/components/ResultsDetail/ResultsDetail.js
@@ -4,34 +4,49 @@ import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
   scoreContainer: {
-    display: 'flex',
-    width: '100%',
-    flexDirection: 'row',
+    display: "flex",
+    width: "80vw",
+    height: "4vw",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    alignItems: "center",
+    justifyContent: "center"
   },
 
   score: {
-    width: '1%',
-    height: '100%',
-    border: '1px solid gray',
-    borderRadius: '2px',
+    width: "3vw",
+    height: "3vw",
+    // border: "1px solid lightgray",
+    borderRadius: "2px",
+    margin: "1px",
+    lineHeight: "3vw",
+    fontSize: "2.8vw",
+    backgroundColor: "whitesmoke",
+    boxShadow: "1px 1px 4px darkgray"
   },
 
   hit: {
-    backgroundColor: 'lightgray',
+    backgroundColor: "gainsboro"
   }
-})
+});
 
 const ResultsDetail = (props) => {
   const { classes } = props;
 
   return (
-  <div className={classes.scoreContainer}>
-    {props.row.original.raw_scores.map(score => {
-      let value = score === 1 ? 'hit' : 'miss';
-      return <div className={classes.score + ' ' + classes + `.${value}`} >{value === 'hit' ? '/' : '0'}</div>
-    })}
-  </div>
-)};
+    <>
+      <div className={classes.scoreContainer}>
+        {props.boxScore.map(score => {
+          let value = score === 1 ? 'hit' : 'miss';
+          return value === 'hit' ? 
+            <div className={`${classes.score} ${classes.hit}`} >/</div> 
+            : 
+            <div className={classes.score} >O</div>
+        })}
+      </div>
+    </>
+  )
+};
 
 
 export default withStyles(styles)(ResultsDetail);
